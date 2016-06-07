d689dede009265ac7e872f5fb4505fdf7bd86cdb
Andrew <andrewyelmanov@gmail.com>
2016-06-07 22:30:05 +0300

    Revert "GoalkeeperAI version2"
    This reverts commit b02b031b3e8f47d8322d8d33b12b17c835f78617.

--- Assets/Scripts/bk/AI/GoalkeeperAI.cs
+++ Assets/Scripts/bk/AI/GoalkeeperAI.cs
@@ -5,24 +5,21 @@ public class GoalkeeperAI : MonoBehaviour {//, Player {
    
     public Transform target; //puck
     [SerializeField]
-    private float speed = 10;
-    private int maxangle = 360;
-    private int minangle = 180;
+    private float speed = 1;
+
 	// Use this for initialization
 	void Start () {
-    }
+        
+	}
 	
 	// Update is called once per frame
 	void Update () {
         Vector3 relativePos = target.position - transform.position;
-        Quaternion rotation = Quaternion.LookRotation(relativePos, new Vector3(0, 1, 0)); //Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(relativePos, new Vector3(0, 1, 0)), speed * Time.deltaTime);
-        // Требуется корректировка, либо сделать по-другоу
-        /*float angle = Mathf.Asin(rotation.y) * Mathf.Rad2Deg - 45;
-        rotation = Quaternion.Euler(0, angle, 0);*/
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
         //Mathf.Lerp(rotation.x, );
-        //Debug.Log(angle);
+        Debug.Log(rotation.x);
         transform.rotation = rotation;
-    }
+	}
 
    /* public string playerName()
     {
